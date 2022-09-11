4473ed91175488be29790ef3c263933f6aacba4f
afv22 <38698043+afv22@users.noreply.github.com>
2022-09-11 18:48:23 -0400

    Organizing User object

--- server/SpiderWeb/schema.py
+++ server/SpiderWeb/schema.py
@@ -1,6 +1,5 @@
 import graphene
 import graphql_jwt
-from graphql import GraphQLError
 
 from SpiderWeb.objects.node.query import NodeQuery
 from SpiderWeb.objects.edge.query import EdgeQuery
@@ -10,15 +9,12 @@ from SpiderWeb.objects.edge.mutation import EdgeMutation
 from SpiderWeb.objects.node.mutation import NodeMutation
 from SpiderWeb.objects.user.mutation import UserMutation
 
-from .objects.user.mutation import *
-
 
 class Query(EdgeQuery, NodeQuery, UserQuery, graphene.ObjectType):
     pass
 
 
 class Mutation(EdgeMutation, NodeMutation, UserMutation, graphene.ObjectType):
-
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
