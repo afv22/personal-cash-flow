653b9f0fa6c24403e897f4cad4b31306dc10c9c7
afv22 <38698043+afv22@users.noreply.github.com>
2022-09-12 12:33:56 -0400

    Migrated usage of ugettext in graphql_jwt to allow for token authentication

--- server/SpiderWeb/objects/user/query.py
+++ server/SpiderWeb/objects/user/query.py
@@ -5,7 +5,7 @@ from graphql import GraphQLError
 
 class UserQuery(graphene.ObjectType):
 
-    whoami = graphene.Field(UserType, token=graphene.String(required=True))
+    whoami = graphene.Field(UserType)
 
     def resolve_whoami(self, info):
         user = info.context.user
